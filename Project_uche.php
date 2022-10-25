1ff485549eccd352a16609924501f1ae378e7a22
UchechiGrace <116463057+UchechiGrace@users.noreply.github.com>
2022-10-25 22:41:59 +0000

    Recent

---
+++ Project_uche.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "storagetank_level";
+
+$con = mysqli_connect($servername,$username,$password,$dbname); 
+
+echo $Water_level=$_GET['Water_level']. "<br/>";                //or POST or PUT
+
+$sql = "INSERT INTO level_reading (Tank_ID, Water_level) VALUES ((SELECT Tank_ID FROM tank_information WHERE Location ='Rooftop'), '{$Water_level}')";  
+//$sql = "INSERT INTO level_reading (Tank_ID, Water_level) VALUES ((SELECT Tank_ID FROM tank_information WHERE Location ='Charlotte'), '{$Water_level}')";     
+
+if (mysqli_query($con, $sql)) {
+echo "New record created successfully";
+} else {
+echo "Unsuccessful";
+}
+
+
+?>
---
+++ Retrievinglevel.php
@@ -0,0 +1,24 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "storagetank_level";
+
+    $con = mysqli_connect($servername,$username,$password,$dbname);
+    
+    $data=array(); 
+   // $q=mysqli_query($con,"SELECT * FROM level_reading");
+
+   $sql = "SELECT * FROM (
+    SELECT * 
+    FROM level_reading 
+    WHERE (Tank_ID = 1)  ORDER BY Reading_ID DESC LIMIT 5
+  ) AS `table` ORDER by Reading_ID ASC";
+
+   if ($q=mysqli_query($con, $sql)) {
+    while ($row = $q->fetch_assoc()){
+        echo "{$row['Water_level']}  {$row['Time']} \n". "<br/>";
+    }
+   } 
+   
+?>
