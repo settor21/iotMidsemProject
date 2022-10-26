d0754a464dd395b33672dab355bd69e099df8a98
UchechiGrace <116463057+UchechiGrace@users.noreply.github.com>
2022-10-26 05:58:23 +0000

    Updated html

---
+++ getlastTank2.php
@@ -0,0 +1,20 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "storagetank_level";
+
+    $con = mysqli_connect($servername,$username,$password,$dbname);
+    
+    $data=array(); 
+   // $q=mysqli_query($con,"SELECT * FROM level_reading");
+
+   $sql = "SELECT * FROM level_reading  WHERE (Tank_ID = 2)  ORDER BY Reading_ID DESC LIMIT 1";
+ 
+   if ($q=mysqli_query($con, $sql)) {
+    while ($row = $q->fetch_assoc()){
+        echo "{$row['Water_level']}  {$row['Time']} \n". "<br/>";
+    }
+   } 
+   
+?>
