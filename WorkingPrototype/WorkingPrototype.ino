0f491fffb55dc73b8c31c3e2d86d262c0e5df4b8
UchechiGrace <116463057+UchechiGrace@users.noreply.github.com>
2022-10-27 15:46:37 +0000

    Working arduino

--- WorkingPrototype/WorkingPrototype.ino
+++ WorkingPrototype/WorkingPrototype.ino
@@ -1,88 +1,239 @@
-/**
- * based on code by Niel Cameron
- * Setup: connect a RED led to pin 21 and a green LED to pin 23
- * YOu may rename your Acess point from ESP32Svr to make it identifiable and uniqe
- * You may change your IP if you wish, but not neccessary.
- * Only difference betwen this an STA version is in the setup of the wireless
- */
-
-
+#include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <ESP32Servo.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
 #include <WebServer.h>
-#include "htmltext.h"
+#include <ESPmDNS.h>
+
+#define USE_SERIAL Serial
+#include "Projectthml.h"
+
+//WiFiMulti wifiMulti;
+Servo myservo;
+#define TRIG_PIN 33 // ESP32 pin GIOP33 connected to Ultrasonic Sensor's TRIG pin
+#define ECHO_PIN 34 // ESP32 pin GIOP34 connected to Ultrasonic Sensor's ECHO pin
+#define LED_PIN  18 // LED pin set on/off based on water level
+#define SDA_PIN 21 // SDA pin for LCD
+#define SCL_PIN 22 // SCL pin for LCD
+#define MOTOR_PIN 13 // Motor pin set on/off based on water level
+#define MAX_DIST 10 //centimeters
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+float duration_us, distance_cm,Water_level;
+int LEDR = LOW;
+const char* ssid = "Uche";
+const char* password = "ashesiclassof2023";
+int manualState  = LOW;
+//int counter = 0;
+
+Servo servo;
 
 WebServer server(80);
-char ssidAP[] = "ESP32AP";
-char passwordAP[] = "12345678"; 
-IPAddress local_ip(192,168,2,1); 
-IPAddress gateway(192,168,2,1); 
-IPAddress subnet(255,255,255,0); 
 
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  }
 
-int LEDGpin = 23;
-int LEDRpin = 21;
+ 
+//  void baseHandle(){
+//    
+//    server.send(200, "text/plain","hello from esp32");
+//    }
 
-int LEDR = LOW;
-int LEDG = LOW;
-int counter = 0;
-void setup()
-{
-  Serial.begin(115200);
-  WiFi.mode(WIFI_AP);
-  delay(1000);
-  WiFi.softAP(ssidAP, passwordAP); 
-  WiFi.softAPConfig(local_ip, gateway, subnet);  // initialise Wi-Fi 
-  server.begin();
-  server.on("/", base);
-  server.on("/LEDGurl",LEDGfunct);
-  server.on("/LEDRurl",LEDRfunct);
-  server.on("/zeroUrl",zerofunct);
-  server.on("/countUrl",countFunct);  //new optional funciton to always update the counter
-  pinMode(LEDGpin, OUTPUT);
-  pinMode(LEDRpin, OUTPUT);
-}
+     void baseHandle(){
+    
+    server.send(200, "text/html",page);
+    }
 
-void base(){
-  server.send(200, "text/html", page);
-}
+   
 
-void LEDGfunct(){
-  LEDG = !LEDG;
-  digitalWrite(LEDGpin, LEDG);
-  counter++;
-  String str = "ON";    //modification below
-  if(LEDG == LOW) str = "OFF";
-  server.send(200, "text/plain", str);
-  Serial.println("green");
-}
-
-//modified to use AJAX
-void LEDRfunct()
-{
-  LEDR = !LEDR;
-  digitalWrite(LEDRpin, LEDR);
-  counter++;
-  String str = "ON";    //very little returned
-  if(LEDR == LOW) str = "OFF";
+  void LEDRfunct()
+{ if (manualState == HIGH){
+  LEDR= !LEDR;
+  digitalWrite(LED_PIN, LEDR);
+  //counter++;
+  String str = "OFF";    //very little returned
+  if(LEDR == HIGH ) str = "ON";
   server.send(200, "text/plain", str);
   Serial.println("red");
 }
-
-void zerofunct() // function to zero counter
-{
-  // and send HTML code to client
-  counter = 0;
-  String str = String(counter);   //only this is sent
-  server.send(200, "text/html", str);
-  Serial.println("zero");
 }
 
-//new optional funciton to always update the counter
-void countFunct() {
-  String str = String(counter);   //only this is sent
-  server.send(200, "text/plain", str);
-  Serial.println("counter");
+void manualfunct(){
+ if(manualState == HIGH){
+// manualState = !manualState
+   manualState = LOW;
 }
+ else{
+  manualState = HIGH;
+ }
+ }
+ 
+ 
+void setup() {
+  // begin serial port
+  Serial.begin (115200); 
+  //pinMode(relay, OUTPUT);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+  // initialize the lcd 
+  lcd.init();                       
+  lcd.clear();
+  lcd.backlight();
 
-void loop() {
-  server.handleClient(); // manage HTTP requests 
+  // configure the trigger pin to output mode
+  pinMode(TRIG_PIN, OUTPUT);
+  // configure the echo pin to input mode
+  pinMode(ECHO_PIN, INPUT);
+  // configure the echo pin to input mode
+  pinMode(LED_PIN, OUTPUT);
+
+  servo.attach(MOTOR_PIN);
+
+  servo.write(0);
+
+
+     for(uint8_t t = 4; t > 0; t--) {
+        Serial.printf("[SETUP] WAIT %d...\n", t);
+        Serial.flush();
+        delay(1000);
+  
+  }
+
+  //wifiMulti.addAP("ssid","password");
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+ 
+    
+     if (MDNS.begin("esp32")) {    //multicast DNS service
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", baseHandle );
+    server.on("/inline", []() {
+    server.send(200, "text/plain", "this works as well");
+  });
+
+  server.onNotFound(handleNotFound);
+  server.on("/client", GET_RQ);
+
+  server.begin();
+  Serial.println("Http server started");
+  server.on("/LEDRurl",LEDRfunct);
+  server.on("/manualurl",manualfunct);
+
 }
+void loop(void) {   
+  server.handleClient();
+  delay(2);
+  // generate 10-microsecond pulse to TRIG pin
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // measure duration of pulse from ECHO pin
+  duration_us = pulseIn(ECHO_PIN, HIGH);
+
+  // calculate the distance
+  distance_cm = 0.017 * duration_us;
+
+
+  Water_level = MAX_DIST - distance_cm;
+ if (manualState == LOW){
+ if (Water_level<=2){
+    digitalWrite(LED_PIN,HIGH);
+    servo.write(360);
+  }
+ 
+  else if (Water_level>=8){
+    digitalWrite(LED_PIN,LOW);
+    servo.write(0);
+  }
+
+  }
+
+  lcd.setCursor(0,0);
+  lcd.print("Water Level");
+  lcd.setCursor(0,1);
+  lcd.print(Water_level); 
+  lcd.setCursor(7,1);
+  lcd.print("cm");
+  
+  Serial.print("Water level: ");
+  Serial.print(Water_level);
+  Serial.println(" cm");
+  Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+ 
+  delay(1500);
+  GET_RQ();
+
+//// Normally Open configuration, send LOW signal to let current flow
+//// (if you're usong Normally Closed configuration send HIGH signal)
+//
+//digitalWrite(relay, LOW);
+//Serial.println("Current Flowing");
+//delay(5000); 
+//
+//// Normally Open configuration, send HIGH signal stop current flow
+//// (if you're usong Normally Closed configuration send LOW signal)
+//digitalWrite(relay, HIGH);
+//Serial.println("Current not Flowing");
+//delay(5000);
+}
+String serverName = "http://192.168.137.1/iotinhtdocs/midsemproject/iotMidsemProject/Project_uche.php?"; 
+void GET_RQ(){
+  if((WiFi.status() == WL_CONNECTED)) {
+
+        HTTPClient http;
+
+        Serial.print("[HTTP] begin...\n");
+        // configure traged server and url
+        String serverPath = serverName + "Water_level="+String(Water_level);
+ 
+        http.begin(serverPath.c_str()); //HTTP
+  
+        Serial.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http.GET();
+        if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                Serial.println(payload);
+                server.send(200, "text/html", payload);
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+  
+  delay(1500);
+ }
--- WorkingPrototype/htmltext.h
+++ WorkingPrototype/htmltext.h
@@ -1,131 +1,41 @@
-/**
- * based on code by Niel Cameron
- * this page forms the text for the HTML
- * Now it is one continous piece because there are no variables
- * Store array (page) data in flash (program) memory instead of SRAM, 
- * qualifeid by the PROGMEM modifier.
- * R"()" is a raw literal string.
- * 
- * This page is returned to client, on the client side, the javascript will
- * execute. THe JS/AJAX code is at end of the file
- */
-
-
-
 char page[] PROGMEM = R"(
-  <!DOCTYPE html><html><head>
-  <title>Local network</title>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Monitoring Storage Tank Level</title>
   <style>
+  .btn {display:block; width:200px; margin:auto; padding:10px}
+  .btn {font-size:20px; color:black; text-decoration:none}
+    .on {background-color:red}
+    .off {background-color:blue}
   
-  .on {background-color:red}
-  .off {background-color:blue}
-  </style></head>
+    </style>
+</head>
+<body>
+  <h1 style="color: blue;">Monitoring Storage Tank Level</h1>
+  <h1 text-align= "center"> </h1>
+  <p style="font-size: 120%;">This page allows user 
+    to control motor for each tank in each house in Regimanuel Estate.</p>
   <body>
-  <h1>ESP32 as server, but also connects to web</h1>
-  <p> Working in STA mode </p>
-  
+
+
   <table style='width:100%'><tr>
-  <td>Green LED is <span id='LEDG'>OFF</span> now</td>
   <td>Red LED is <span id='LEDR'>OFF</span> now</td>
   </tr></table>
-  
   <table style='width:100%'><tr>
-  <td><button class = 'btn off' id='Green LED'
-       onclick = 'sendData(id)'>Press to turn Green LED ON
-       </button></td>
-       <td><button class = 'btn off' id='Red LED'
-     onclick = 'sendData(id)'>Press to turn Red LED ON
-     </button></td>
-</tr></table>
-<p>Counter is <span id='counter'>0</span> now</p>
-<button class = 'btn zero' id = 'zero'
-     onclick = 'sendData(id)'>Press to zero counter</button>
 
-<div id="extText"> </div>
+  <td><button class = 'btn off' id='manual'
+     onclick = 'sendData(id)'>Press to enter manual operation
+     </button></td>
      
-<script>
-function sendData(butn)
-{
-  var URL, variab, text;
-  if(butn == 'Red LED')
-  {
-    URL = 'LEDRurl';
-    variab = 'LEDR';
-  }
-  else if(butn == 'Green LED')
-  {
-    URL = 'LEDGurl';
-    variab = 'LEDG';
-  }
-  else if(butn == 'zero')
-  {
-    URL = 'zeroUrl';
-    variab = 'counter';
-  }
-  if(butn == 'Red LED' || butn == 'Green LED')
-{ // change button class and text 
-  var state = document.getElementById(butn).className;
-  state = (state == 'btn on' ? 'btn off' : 'btn on');
-  text = (state == 'btn on' ? butn + ' OFF' : butn + ' ON');
-    document.getElementById(butn).className = state;
-    document.getElementById(butn).innerHTML = 'Press to turn ' + text;
-  }
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function(){
-    document.getElementById(variab).innerHTML = this.responseText;
-  }
-  xhr.open('GET', URL, true);
-  xhr.send();
-}
-
-
-<!-- not needed, but enables periodic update of count -->
-/**
- * based on code by Niel Cameron
- * this page forms the text for the HTML
- * Now it is one continous piece because there are no variables
- * Store array (page) data in flash (program) memory instead of SRAM, 
- * qualifeid by the PROGMEM modifier.
- * R"()" is a raw literal string.
- * 
- * This page is returned to client, on the client side, the javascript will
- * execute. THe JS/AJAX code is at end of the file
- */
-
-
-
-char page[] PROGMEM = R"(
-  <!DOCTYPE html><html><head>
-  <title>Local network</title>
-  <style>
-  body {margin-top:50px; font-family:Arial}
-  body {font-size:20px; text-align:center}
-  .btn {display:block; width:280px; margin:auto; padding:30px}
-  .btn {font-size:30px; color:black; text-decoration:none}
-  .on {background-color:SkyBlue}
-  .off {background-color:LightSteelBlue}
-  .zero {background-color:Thistle}
-  td {font-size:30px; margin-top:50px; margin-bottom:5px}
-  p {font-size:30px; margin-top:50px; margin-bottom:5px}
-  </style></head>
-  <body>
-  <h1>ESP32 local area network</h1>
-  <table style='width:100%'><tr>
-  <td>Green LED is <span id='LEDG'>OFF</span> now</td>
-  <td>Red LED is <span id='LEDR'>OFF</span> now</td>
-  </tr></table>
-  <table style='width:100%'><tr>
-  <td><button class = 'btn off' id='Green LED'
-       onclick = 'sendData(id)'>Press to turn Green LED ON
-       </button></td>
-       <td><button class = 'btn off' id='Red LED'
-     onclick = 'sendData(id)'>Press to turn Red LED ON
+  <td><button class = 'btn off' id='Red LED'
+     onclick = 'sendData(id)'>Press to turn LED ON
      </button></td>
+
+     
 </tr></table>
-<p>Counter is <span id='counter'>0</span> now</p>
-<button class = 'btn zero' id = 'zero'
-     onclick = 'sendData(id)'>Press to zero counter</button>
-<script>
+<script> 
+
 function sendData(butn)
 {
   var URL, variab, text;
@@ -134,21 +44,20 @@ function sendData(butn)
     URL = 'LEDRurl';
     variab = 'LEDR';
   }
-  else if(butn == 'Green LED')
-  {
-    URL = 'LEDGurl';
-    variab = 'LEDG';
-  }
-  else if(butn == 'zero')
-  {
-    URL = 'zeroUrl';
-    variab = 'counter';
-  }
-  if(butn == 'Red LED' || butn == 'Green LED')
+
+  if(butn == 'manual')
+{
+ URL = 'manualurl';
+  variab = 'manualState';
+    
+ }
+
+
+  if(butn == 'Red LED')
 { // change button class and text 
   var state = document.getElementById(butn).className;
   state = (state == 'btn on' ? 'btn off' : 'btn on');
-  text = (state == 'btn on' ? butn + ' OFF' : butn + ' ON');
+  text = (state == 'btn on' ? butn + 'OFF' : butn + ' ON');
     document.getElementById(butn).className = state;
     document.getElementById(butn).innerHTML = 'Press to turn ' + text;
   }
@@ -156,25 +65,11 @@ function sendData(butn)
   xhr.onreadystatechange = function(butn)
   {
     if (this.readyState == 4 && this.status == 200)
-    document.getElementById(variab).innerHTML = this.
-    responseText;
+
+//   xhr.onload = function(){
+    document.getElementById(variab).innerHTML = this.responseText;
   };
   xhr.open('GET', URL, true);
-  xhr.send();
-}
-
-<!-- not needed, but enables periodic update of count -->
-setInterval(reload, 1000);
-function reload()
-{
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function()
-  {
-    if(this.readyState == 4 && this.status == 200)
-    document.getElementById('counter').innerHTML = this.
-    responseText;
-  };
-  xhr.open('GET', 'countUrl', true);
   xhr.send();
 }
 </script>
